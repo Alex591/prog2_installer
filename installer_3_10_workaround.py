a469c4478824ece1443f8fc69f758290388e8b64
Alex Adam <alex.adam@ni.com>
2022-09-15 21:13:13 +0200

    install independent version

--- installer_3_10_workaround.py
+++ installer_3_10_workaround.py
@@ -20,10 +20,6 @@ def run_batch_file(file_path):
     Popen(file_path, creationflags=subprocess.CREATE_NEW_CONSOLE)
 
 
-def check_for_conda() -> bool:
-    # ha conda környezet van akkor másképp néz ki a fájlstruktúra ahol az exe van
-    return os.path.exists(os.path.join(sys.prefix, 'conda-meta'))
-
 
 if __name__ == '__main__':
     # telepités
@@ -38,29 +34,24 @@ if __name__ == '__main__':
 
     ##create desktop shortcut
 
-    qt_designer_location = sys.executable
-    # sys.executable
-    # A string giving the absolute path of the executable binary for the Python interpreter, on systems where this makes sense.
-    # If Python is unable to retrieve the real path to its executable, sys.executable will be an empty string or None
-    conda = check_for_conda()
-    hossz = len(qt_designer_location) - 18 if not conda else len(qt_designer_location) - 10
-    qt_designer_location = qt_designer_location[:hossz]
-    # a sys.executeable által visszaadott stringből levágjuk a "Scripts\python.exe" vagy a "python.exe"-t
-    # conda estében a "python.exe"-t kell csak levágni
-    qt_designer_location = qt_designer_location + 'Lib\site-packages\qt6_applications\Qt\\bin\\designer.exe'
-    print(qt_designer_location)
+    try:
+        import qt6_applications as _
+        qt_designer_location=_.__path__[0]+"\\Qt\\bin\\designer.exe"
 
-    script = open("script.bat", "w")
-    print("@echo off", file=script)
-    print('set SCRIPT="%TEMP%\%RANDOM%-%RANDOM%-%RANDOM%-%RANDOM%.vbs"', file=script)
-    print('echo Set oWS = WScript.CreateObject("WScript.Shell") >> %SCRIPT%', file=script)
-    print('echo sLinkFile = "%USERPROFILE%\Desktop\QT Designer.lnk" >> %SCRIPT%', file=script)
-    print('echo Set oLink = oWS.CreateShortcut(sLinkFile) >> %SCRIPT%', file=script)
-    print(f'echo oLink.TargetPath = "{qt_designer_location}" >> %SCRIPT%', file=script)
-    print('echo oLink.Save >> %SCRIPT%', file=script)
-    print('cscript /nologo %SCRIPT%', file=script)
-    print('del %SCRIPT%', file=script)
-    print('DEL "%~f0"', file=script)
-    script.close()
-    scriptdir = os.getcwd() + "\script.bat"
-    run_batch_file('script.bat')
+        script = open("script.bat", "w")
+        print("@echo off", file=script)
+        print('set SCRIPT="%TEMP%\%RANDOM%-%RANDOM%-%RANDOM%-%RANDOM%.vbs"', file=script)
+        print('echo Set oWS = WScript.CreateObject("WScript.Shell") >> %SCRIPT%', file=script)
+        print('echo sLinkFile = "%USERPROFILE%\Desktop\QT Designer.lnk" >> %SCRIPT%', file=script)
+        print('echo Set oLink = oWS.CreateShortcut(sLinkFile) >> %SCRIPT%', file=script)
+        print(f'echo oLink.TargetPath = "{qt_designer_location}" >> %SCRIPT%', file=script)
+        print('echo oLink.Save >> %SCRIPT%', file=script)
+        print('cscript /nologo %SCRIPT%', file=script)
+        print('del %SCRIPT%', file=script)
+        print('DEL "%~f0"', file=script)
+        script.close()
+        scriptdir = os.getcwd() + "\script.bat"
+        run_batch_file('script.bat')
+    except Exception as e: print(e)
+        
+
